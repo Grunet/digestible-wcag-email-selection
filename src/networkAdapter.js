62c7c415a7495b6eeb6b5686701a1073df217152
Grunet <sadikshahidain@gmail.com>
2020-09-13 20:48:52 -0500

    Adding in a not implemented exception in anticipation of S3 not implementing all of the methods

--- src/networkAdapter.js
+++ src/networkAdapter.js
@@ -6,26 +6,34 @@ class NetworkAdapter {
   }
 
   async getTextFileContents(inputs) {
-    const adapter = this.__getAdapterToUse(inputs);
+    const { adapter, key: adapterKey } = this.__getAdapterToUse(inputs);
 
     let adapterInputs = [];
-    switch (adapter) {
-      case this.__adapters.fetch:
+    switch (adapterKey) {
+      case "fetch":
         adapterInputs = [inputs.url];
         break;
+      default:
+        throw new Error(
+          `${adapterKey}'s adapter hasn't been implemented yet for this method`
+        );
     }
 
     return await adapter.getTextFileContents(...adapterInputs);
   }
 
   async getJsonFileAsJsObj(inputs) {
-    const adapter = this.__getAdapterToUse(inputs);
+    const { adapter, key: adapterKey } = this.__getAdapterToUse(inputs);
 
     let adapterInputs = [];
-    switch (adapter) {
-      case this.__adapters.fetch:
+    switch (adapterKey) {
+      case "fetch":
         adapterInputs = [inputs.url];
         break;
+      default:
+        throw new Error(
+          `${adapterKey}'s adapter hasn't been implemented yet for this method`
+        );
     }
 
     return await adapter.getJsonFileAsJsObj(...adapterInputs);
@@ -33,7 +41,7 @@ class NetworkAdapter {
 
   __getAdapterToUse(inputs) {
     if ("url" in inputs) {
-      return this.__adapters.fetch;
+      return { key: "fetch", adapter: this.__adapters.fetch };
     }
 
     throw new Error("Unrecognized inputs: ", inputs);
