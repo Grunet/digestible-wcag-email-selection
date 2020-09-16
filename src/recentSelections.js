0f807fe2c85edc9410e5ecfc9b218b138312a24a
Grunet <sadikshahidain@gmail.com>
2020-09-15 22:07:27 -0500

    Fleshing out recentSelections.js using the methods from networkAdapter.js

---
+++ src/recentSelections.js
@@ -0,0 +1,61 @@
+const { createNetworkAdapter } = require("./networkAdapter.js");
+
+const defaulStorageIds = {
+  bucket: "digestible-wcag-email-selection",
+  filename: "recentSelections.json",
+};
+
+class RecentSelectionsProxy {
+  constructor(inputs) {
+    const { networkAdapter, storageIds } = inputs;
+
+    this.__networkAdapter = networkAdapter ?? createNetworkAdapter();
+    this.__storageIds = Object.assign({ ...defaulStorageIds }, storageIds);
+  }
+
+  async getAll() {
+    const { recentSelections } = await this.__networkAdapter.getJsonFileAsJsObj(
+      {
+        ...this.__storageIds,
+      }
+    );
+
+    return recentSelections;
+  }
+
+  async clearAll() {
+    await this.__networkAdapter.setJsObjIntoJsonFile({
+      ...this.__storageIds,
+      obj: { recentSelections: [] },
+    });
+  }
+
+  async add(selectionData) {
+    const { id } = selectionData;
+
+    const recentSelections = await this.getAll();
+
+    const updatedRecentSelections = recentSelections.concat([
+      {
+        id: id,
+        timestamp: new Date().toJSON(),
+      },
+    ]);
+
+    await this.__networkAdapter.setJsObjIntoJsonFile({
+      ...this.__storageIds,
+      obj: { recentSelections: updatedRecentSelections },
+    });
+  }
+}
+
+function setupService(inputs) {
+  return new RecentSelectionsProxy({
+    networkAdapter: inputs?.dependencies?.networkAdapter,
+    storageIds: inputs?.storageIds,
+  });
+}
+
+module.exports = {
+  setupService: setupService,
+};
